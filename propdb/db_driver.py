fixed path finding issue, added krr calculation of potential

propdb/db_driver.py
@@ -2,12 +2,14 @@ import numpy as np
 from time import time
 from typing import List, Optional
 from .compute_path import mintree
-from .krr import make_krr_weights
+from .krr import make_krr_weights,make_vkrr
 from .prop_diabatization import compute_cmat,compute_cmat_krr
 
 def diabatize(nel: int,
               ngeoms: int,
+              nmodes: int,
               geoms: np.ndarray,
+              modes: np.ndarray,
               eads: np.ndarray,
               Fs: np.ndarray,
               krrflag: Optional[bool] = False,
@@ -20,7 +22,7 @@ def diabatize(nel: int,
   # TODO do some checks about the order of things
 
   # compute nearest-neighbor path
-  nns = mintree(ngeoms, geoms)
+  nns = mintree(ngeoms, modes.T)
 
   # initialize adiabatic-to-diabatic rotation matrices
   Cmats = np.zeros((ngeoms,nel,nel))
@@ -32,26 +34,29 @@ def diabatize(nel: int,
     every = 1
 
   if krrflag:
-    # do diabatization with krr
-    nmodes = krrargs[0]
-    modes  = krrargs[1]
-    if len(krrargs) == 2:
+    if krrargs is None:
       # default number of integration steps
       nsteps    = 40
       # default krr parameters
       alpha     = 0.5
       gam       = 1.e-4
       modetypes = ['gaussian' for i in range(nmodes)]
-    elif len(krrargs) == 6:
-      nsteps    = krrargs[2]
-      alpha     = krrargs[3]
-      gam       = krrargs[4]
-      modetypes = krrargs[5]
+    elif len(krrargs) == 5:
+      nsteps    = krrargs[0]
+      alpha     = krrargs[1]
+      gam       = krrargs[2]
+      modetypes = krrargs[3]
+      vkrrflag  = krrargs[4]
     else:
       raise ValueError("Incorrect number of arguments for krrargs--must be 2 or 6 arguments.")
 
     # find krr weights for adiabatic energies
     krr_weights = make_krr_weights(nel,nmodes,ngeoms,modes,eads,alpha,gam,modetypes)
+
+    # print the potential from krr
+    if vkrrflag:
+      vkrr = make_vkrr(nmodes,ngeoms,modes,alpha,modetypes,krr_weights)
+      np.save('vkrr.npy',vkrr,allow_pickle=True)
 
     # do diabatization
     btime = time()